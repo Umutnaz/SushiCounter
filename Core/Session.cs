0b1785c6a260d91d32c7bb0eeecf3bdf9f35171e
Umutnaz <umutnaz@outlook.dk>
2025-10-30 22:46:55 +0100

    hej

---
+++ Core/Session.cs
@@ -0,0 +1,10 @@
+﻿namespace Core;
+
+public class Session
+{
+    public int SessionId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int userId { get; set; }
+}
