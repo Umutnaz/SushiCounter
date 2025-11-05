1cd0d231b19057f762edde2f6b2fe0caa2c9ca1c
Umutnaz <umutnaz@outlook.dk>
2025-11-05 12:46:01 +0100

    fikset premissions ved CreateSession.razor modal

--- Frontend/Service/IService/ISessionService.cs
+++ Frontend/Service/IService/ISessionService.cs
@@ -5,8 +5,8 @@ namespace Frontend.Service.IService;
 public interface ISessionService
 {
     Task<List<Session>> GetMySessionsAsync(string userId);      // Hent alle sessioner for én user (creator eller deltager)
-    Task<List<Session>> GetMyOpenSessionsAsync(string userId);  // Hent åbne sessioner for én user (til SushiCounter)
-    Task<Session?> GetSessionBySessionIdAsync(string sessionId);
+    Task<List<Session>> GetMyOpenSessionsAsync(string userId);  // nok ik nødvendigt heller
+    Task<Session?> GetSessionBySessionIdAsync(string sessionId); // ved ikke om det nødvendigt
     Task<Session?> AddSessionAsync(string creatorUserId, Session session);
     Task<bool> UpdateSessionAsync(Session session);
     Task<bool> DeleteSessionAsync(string sessionId); // skal man kunne slette en session?
